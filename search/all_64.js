140b724def8aabe198fed8596ea3027e266ece53
danielmcinnes <d_h_mcinnes@hotmail.com>
2014-01-20 08:42:49 +1000

    added a html page documenting requirements

--- search/all_64.js
+++ search/all_64.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['data',['data',['../structqt__meta__stringdata__ModelView__t.html#afac529de2d6104664993390f14b2a97b',1,'qt_meta_stringdata_ModelView_t']]],
   ['database',['Database',['../classDatabase.html',1,'Database'],['../classDatabase.html#aac1f583197aa119e05528ebb4e3d7151',1,'Database::Database()']]],
   ['database_2ecpp',['database.cpp',['../database_8cpp.html',1,'']]],
   ['database_2eh',['database.h',['../database_8h.html',1,'']]],
--- search/all_73.js
+++ search/all_73.js
@@ -6,6 +6,5 @@ var searchData=
   ['save_2eh',['save.h',['../save_8h.html',1,'']]],
   ['serialize',['serialize',['../classCube.html#ab81cb581ae5959ef0cdf4462321ae496',1,'Cube::serialize()'],['../classLatitudes.html#abcd69097e6d048d77a4db596a8baee04',1,'Latitudes::serialize()'],['../classLongitudes.html#a405d36bcf2eb92e4c731178b9e6c8c23',1,'Longitudes::serialize()'],['../classLocation.html#a44793ff604a4f27c36114fb32e31835d',1,'Location::serialize()'],['../classLocations.html#aec8627451d5662d552db48e971fb7c80',1,'Locations::serialize()']]],
   ['simpleplot_2ecpp',['simpleplot.cpp',['../simpleplot_8cpp.html',1,'']]],
-  ['simpleplot_2eh',['simpleplot.h',['../simpleplot_8h.html',1,'']]],
-  ['stringdata',['stringdata',['../structqt__meta__stringdata__ModelView__t.html#ab89b330bf3b480cd9b3f53deccf20463',1,'qt_meta_stringdata_ModelView_t']]]
+  ['simpleplot_2eh',['simpleplot.h',['../simpleplot_8h.html',1,'']]]
 ];
